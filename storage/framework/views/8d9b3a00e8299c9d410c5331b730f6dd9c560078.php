506e42adff0a907744a6a42e9abb6bf3286a9d6c
dadop <davorpeu@gmail.com>
2019-09-11 15:55:46 +0200

    Added repo

---
+++ storage/framework/views/8d9b3a00e8299c9d410c5331b730f6dd9c560078.php
@@ -0,0 +1,39 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+<?php $__env->startSection('content'); ?>
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://instagram.fzag1-1.fna.fbcdn.net/vp/72dccf2411a5e67b8e3dedcf31ec153f/5DC78187/t51.2885-19/s150x150/39381491_2016744545013825_6121577575153139712_n.jpg?_nc_ht=instagram.fzag1-1.fna.fbcdn.net" class="rounded-circle">
+        </div>
+        <!--<editor-fold desc="hey prvi komentar">-->
+        <div class="col-9 pt-5">
+            <div><h1><?php echo e($user->username); ?></h1></div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>57</strong> posts</div>
+                <div class="pr-5"><strong>110</strong> followers</div>
+                <div class="pr-5"><strong>268</strong> following</div>
+            </div>
+            <div class="pt-4 font-weight-bold">Glinar Gleesho</div>
+            <div>🗝️ ° ⚔️ ° 🏹
+                I make miniatures for tabletop gaming. 👀🤲
+                25, Croatia
+                Contact me:clayminiaturecrafter@gmail.com</div>
+            <div class="font-weight-bold "><a href="www.reddit.com/user/clayminicrafter">www.reddit.com/user/clayminicrafter</a></div>
+        </div>
+        <!--</editor-fold>-->
+    </div>
+    <div class="row" pt-5>
+        <div class="col-4">
+            <img src="https://instagram.fzag1-1.fna.fbcdn.net/vp/f1e340a8ea6cd171ef4a65b6b707f12f/5DD9D419/t51.2885-15/sh0.08/e35/s640x640/59883333_455285081907214_1789233363913545963_n.jpg?_nc_ht=instagram.fzag1-1.fna.fbcdn.net" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://instagram.fzag1-1.fna.fbcdn.net/vp/99e633c9773c803df9f19f0cae3694e3/5DD7BE36/t51.2885-15/sh0.08/e35/s640x640/59330351_629029664280392_4225450268599073772_n.jpg?_nc_ht=instagram.fzag1-1.fna.fbcdn.net" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://instagram.fzag1-1.fna.fbcdn.net/vp/c749bdf2aa5e969f99309f27449eec54/5DE92239/t51.2885-15/sh0.08/e35/s640x640/56900922_115004779680770_2807598313109423157_n.jpg?_nc_ht=instagram.fzag1-1.fna.fbcdn.net" class="w-100">
+        </div>
+    </div>
+</div>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\Dado\Documents\GitHub\freCodeGram\resources\views/home.blade.php ENDPATH**/ ?>
